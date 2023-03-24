f304d2e846e1bae42ddd0cebe08288f6aaa4d1aa
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-03-24 13:36:44 -0700

    added some styling

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='App-footer'>
+    Because I'm bored
+    </footer>
+  )
+}
+
+export default Footer
--- src/layouts/GlobalLayout.js
+++ src/layouts/GlobalLayout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 const GlobalLayout = () => {
   return (
-    <div>
+    <div className='GlobalLayout'>
     <NavBar/>
     <Outlet/>
+    <Footer/>
     </div>
   )
 }
