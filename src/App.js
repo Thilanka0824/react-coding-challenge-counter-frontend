511da8f3cdf11b1ca4cb123e62e10e84c5628659
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-03-23 23:14:33 -0700

    logic portion complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App App-header">
+   <Counter/>
+   <img src="https://www.nicepng.com/png/detail/19-193692_mega-charizard-png-mega-charizard-y.png" alt="Mega Charizard Png - Mega Charizard Y@nicepng.com"></img>
+    </div>
+  );
+}
+
+export default App;
